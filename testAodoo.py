7bedc8b1df30dc6de6b16f72d7a5514e12033fb8
tax25 <andreatax25@gmail.com>
2024-04-25 21:33:56 +0200

    From tabs to spaces

--- testAodoo.py
+++ testAodoo.py
@@ -6,33 +6,33 @@ import subprocess
 
 def main(arguments: list):
 
-	if sys.argv[1] == 'undo':
-		testCase = int(sys.argv[2]) + 10
+    if sys.argv[1] == 'undo':
+        testCase = int(sys.argv[2]) + 10
 
-	elif isinstance(sys.argv[1], int):
-		testCase = sys.argv[1]
-	
-	else:
-		print("Wrong command line arguments...")
-		exit()
+    elif isinstance(sys.argv[1], int):
+        testCase = sys.argv[1]
+    
+    else:
+        print("Wrong command line arguments...")
+        exit()
 
-	match testCase:
-		case 1:
-			# generation case
-			subprocess.run(['./aodoo.py', 'generate', 'module', 'my_test_module', 'True'])
+    match testCase:
+        case 1:
+            # generation case
+            subprocess.run(['./aodoo.py', 'generate', 'module', 'my_test_module', 'True'])
 
-		case 11:
-			subprocess.run(['rm', '-rf', 'my_test_module'])
+        case 11:
+            subprocess.run(['rm', '-rf', 'my_test_module'])
 
-		case _:
-			print("Test case not yet implemented...")
-			exit()
+        case _:
+            print("Test case not yet implemented...")
+            exit()
 
 
 if __name__ == "__main__":
-	
-	if len(sys.argv) == 1:
-		print("No values have been passed")
-		exit()
+    
+    if len(sys.argv) == 1:
+        print("No values have been passed")
+        exit()
 
-	main(sys.argv)
+    main(sys.argv)
