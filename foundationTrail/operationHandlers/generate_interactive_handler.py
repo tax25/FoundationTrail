8828f8141e39b1ddce57d043a47cd0538b54e417
tax25 <andreatax25@gmail.com>
2026-02-02 10:34:12 +0100

    Updates regarding the interactive mode
    Edits in generate_interactive_handler (might have to review the name of the file) in order to handle the fields to request to the user in a better way. More work is needed, but we're getting there.

--- foundationTrail/operationHandlers/generate_interactive_handler.py
+++ foundationTrail/operationHandlers/generate_interactive_handler.py
@@ -1,12 +1,14 @@
-ERROR_TYPE_NOT_COMPATIBLE = 'The value you specified cannot be converted to the needed type.'
+ERROR_TYPE_NOT_COMPATIBLE = 'The value you specified cannot be converted into the needed type.'
 
 def _check_value_is_type(value_to_check, type_to_check_against) -> bool:
-    if not type_to_check_against.get('__name__', ''):
+    # NOTE: checking if `type_to_check_against` is actually a type
+    # and not a random value
+    if not isinstance(type_to_check_against, type):
         # TODO: also print on screen some kind of warning to inform that the type was not specified.
         return False 
 
-    if isinstance(value_to_check, type_to_check_against):
-        return True
+    if not isinstance(value_to_check, type_to_check_against):
+        return False
     
     return True
 
@@ -18,7 +20,7 @@ def handle_generate_interactive(params_to_handle: list[dict]) -> dict:
             {
                 'property_name': 'technical_name_of_the_property', # a.k.a the name of the parameter for the function that will be called.
                 'property_type': str | int | float, # etc...
-                'property_ids_optional': True, # can the user avoid giving this value to us?
+                'property_is_optional': True, # can the user avoid giving this value to us?
                 'property_allowed_vals': [], # a list of allowed values, to which the user-inserted value will be compared to.
                 'property_ask_for_val_msg': '', # the string that will be printed to screen to ask the user for the value.
             }
@@ -26,21 +28,32 @@ def handle_generate_interactive(params_to_handle: list[dict]) -> dict:
     '''
 
     # TODO: include value check on all parts of the list 
-    
+
     return_dict = {}
     for param in params_to_handle:
-        ask_for_val_msg = param.get('property_ask_for_val_msg',f"Please insert value for: '{param.get('property_label', '')}':")
-        value = input(f'{ask_for_val_msg}: ')
-        
-        if not _check_value_is_type(value, param.get('property_type', str)):
+        fallback_string = f"Please insert value for '{param.get('property_label', '')}':"
+        ask_for_val_msg = param.get('property_ask_for_val_msg', fallback_string)
+        # final_msg = f"{ask_for_val_msg}{' [Optional]' if param['property_is_optional'] else ''}{' [y/N]' if param['property_type'] is bool else ''}:"
+        final_msg = '{}{}{}'.format(
+            ask_for_val_msg,
+            ' [Optional]' if param['property_is_optional'] else '',
+            ' [y/N]' if param['property_type'] is bool else ''
+        )
+
+        value = input(final_msg)
+
+        if value != '' and param['property_type'] is not bool and not _check_value_is_type(value, param.get('property_type', str)):
             print(ERROR_TYPE_NOT_COMPATIBLE)
 
         if param.get('property_allowed_vals', []):
             if value not in param['property_allowed_vals']:
                 print("value not between allowed values.")
 
-        return_dict[param['property_name']] = value
-    
-    print('valore di return_dict: ', return_dict)
+        if param['property_type'] is not bool:
+            return_dict[param['property_name']] = value
+        else:
+            return_dict[param['property_name']] = True if value in ('y', 'Y') else False
+
+    # print('[DEBUG] return_dict's value: ', return_dict)
 
     return return_dict
