c867468110f1ebee167ebccff5152aa622a9e1e4
tax25 <andreatax25@gmail.com>
2026-05-13 23:37:54 +0200

    Refactoring __main__.py
    New argparser argument handling

--- foundationTrail/__main__.py
+++ foundationTrail/__main__.py
@@ -34,61 +34,7 @@ def foundationTrail_entrypoint() -> None:
         send_help()
         sys.exit()
     
-    parser = argparse.ArgumentParser(
-        prog='FoundationTrail',
-        description='A tool for Odoo developing',
-        epilog='Stay the reading of our swan song and epilogue',
-        add_help=False
-    )
-    
-    _ = parser.add_argument_group('Miscellaneous')
-    _ = parser.add_argument('--help', action='store_true')
-    _ = parser.add_argument('-V', '--version', action='store_true')
-    _ = parser.add_argument('-I', '--interactive', action='store_true')
-    _ = parser.add_argument('-e', '--explain', type=str)
-    
-    _ = parser.add_argument('-n', '--name', type=str)
-    _ = parser.add_argument('-fn', '--filename', type=str)
-
-    _ = parser.add_argument_group('Basic Actions')
-    _ = parser.add_argument('-g', '--generate', action='store_true')
-
-    _ = parser.add_argument_group('Specifics')
-    _ = parser.add_argument_group('Modules')
-    _ = parser.add_argument('-M', '--module', action='store_true')
-    _ = parser.add_argument('-a', '--app', action='store_true')
-    _ = parser.add_argument('-d', '--deps', type=str)
-    _ = parser.add_argument('-A', '--author', type=str)
-    _ = parser.add_argument('-mv', '--m-version', type=str)
-    _ = parser.add_argument('-D', '--description', type=str)
-    _ = parser.add_argument('-c', '--category', type=str)
-
-    _ = parser.add_argument_group('Models')
-    _ = parser.add_argument('-m', '--model', action='store_true')
-    _ = parser.add_argument('-mt', '--model-type', type=str)
-    _ = parser.add_argument('-i', '--inherit', type=str)
-    _ = parser.add_argument('-wz', '--wizard', action='store_true')
-    _ = parser.add_argument('-mp', '--m-perms', type=str)
-
-    _ = parser.add_argument_group('Views')
-    _ = parser.add_argument('-v', '--view', action='store_true')
-    _ = parser.add_argument('-vm', '--view-model', type=str)
-    _ = parser.add_argument('-wv', '--wizard-view', action='store_true')
-    _ = parser.add_argument('-iv', '--inherit-view', type=str)
-    
-    _ = parser.add_argument_group('Security')
-    _ = parser.add_argument('-s', '--security', action='store_true')
-    _ = parser.add_argument('-id', '--line-id', type=str)
-    _ = parser.add_argument('-ln', '--line-name', type=str)
-    _ = parser.add_argument('-mid', '--model-id', type=str)
-    _ = parser.add_argument('-gid', '--group-id', type=str)
-    _ = parser.add_argument('-pr', '--perm-read', action='store_true')
-    _ = parser.add_argument('-pw', '--perm-write', action='store_true')
-    _ = parser.add_argument('-pc', '--perm-create', action='store_true')
-    _ = parser.add_argument('-pu', '--perm-unlink', action='store_true')
-    
     cli_args = FTArguments()
-    _ = parser.parse_args(namespace=cli_args)
     
     if cli_args.version:
         print(
@@ -114,9 +60,9 @@ def foundationTrail_entrypoint() -> None:
     
     if cli_args.generate:
         if cli_args.module:
-            handle_generate_module(**cli_args.get_module_props_in_dict())
+            handle_generate_module(**cli_args.fn_get_module_props_in_dict())
         elif cli_args.model:
-            handle_generate_model(**cli_args.get_model_props_in_dict())
+            handle_generate_model(**cli_args.fn_get_model_props_in_dict())
         elif cli_args.view:
             pass
         elif cli_args.security:
--- foundationTrail/utils.py
+++ foundationTrail/utils.py
@@ -1,5 +1,80 @@
+from argparse import ArgumentParser
 from typing import override
 
+class Argument:
+    shortForm: str = ''
+    longForm: str = ''
+    argumentType: type = type
+    action: str = ''
+    
+    def __init__(self,
+             short: str, 
+             long: str, 
+             argType: type = type,
+             action: str=''
+    ):
+        self.shortForm = short
+        self.longForm = long
+        self.argumentType = argType
+        self.action = action
+
+
+# NOTE:
+'''
+To be able to use the groups of argparse, this has to be done:
+    ```
+        parser = argparse.ArgumentParser()
+        my_group = parser.add_argument_group('groupName')
+        my_group.add_argument(**my_argument)
+    ```
+'''
+
+ARGUMENTS_DESCR = {
+    'Miscellaneous': [
+        Argument('-h', '--help', action='store_true'),
+        Argument('-V', '--version', action='store_true'),
+        Argument('-I', '--interactive', action='store_true'),
+        Argument('-e', '--explain', argType=str),
+        Argument('-n', '--name', argType=str),
+        Argument('-fn', '--filename', argType=str),
+    ],
+    'Basic Actions': [
+        Argument('-g', '--generate', action='store_true'),
+    ],
+    'Specifics.Modules': [
+        Argument('-M', '--module', action='store_true'), 
+        Argument('-a', '--app', action='store_true'),
+        Argument('-d', '--deps', argType=str),
+        Argument('-A', '--author', argType=str),
+        Argument('-mv', '--m_version', argType=str),
+        Argument('-D', '--description', argType=str),
+        Argument('-c', '--category', argType=str),
+    ],
+    'Specifics.Models': [
+        Argument('-m', '--model', action='store_true'), 
+        Argument('-mt', '--model_type', argType=str),
+        Argument('-i', '--inherit', argType=str),
+        Argument('-wz', '--wizard', action='store_true'),
+        Argument('-mp', '--m_perms', argType=str)
+    ],
+    'Specifics.Views': [
+        Argument('-v', '--view', action='store_true'),
+        Argument('-vm', '--view_model', argType=str),
+        Argument('-wv', '--wizard_view', action='store_true'),
+        Argument('-iv', '--inherit_view', argType=str),
+    ],
+    'Specifics.Security': [
+        Argument('-s', '--security', action='store_true'),
+        Argument('-id', '--line_id', argType=str),
+        Argument('-ln', '--line_name', argType=str),
+        Argument('-mid', '--model_id', argType=str),
+        Argument('-gid', '--group_id', argType=str),
+        Argument('-pr', '--perm-read', action='store_true'),
+        Argument('-pw', '--perm-write', action='store_true'),
+        Argument('-pc', '--perm-create', action='store_true'),
+        Argument('-pu', '--perm-unlink', action='store_true'),
+    ],
+}
 
 class FTArguments:
     help: bool = False
@@ -41,7 +116,67 @@ class FTArguments:
     perm_create: bool = False
     perm_unlink: bool = False
     
-    def get_module_props_in_dict(self) -> dict[str, str | bool]:
+    _parser: ArgumentParser = ArgumentParser(
+        prog='FoundationTrail',
+        description='A tool for Odoo developing',
+        epilog='Stay the reading of our swan song and epilogue',
+        add_help=False
+    )
+    
+    def _fn_get_class_props_list(self) -> list[str]:
+        all_props: list[str] = []
+        for prop in __class__.__dict__.items():
+            if (prop[0].startswith('_') or prop[0].startswith('fn_')):
+                continue
+
+            all_props.append(prop[0])
+
+        return all_props
+    
+    def _fn_flatten_args_descr(self, args_descr: dict[str, list[Argument]]):
+        flattened: list[Argument] = []
+        for group in args_descr:
+            for argument in args_descr[group]:
+                flattened.append(argument)
+        
+        return flattened
+
+
+    def __init__(self):
+        cli_args: list[Argument] = self._fn_flatten_args_descr(ARGUMENTS_DESCR)
+
+        print("valore cli_args: ", cli_args, len(cli_args))
+
+        class_props = self._fn_get_class_props_list()
+        
+        print("valore class_props: ", class_props, len(class_props))
+
+        if len(class_props) != len(cli_args):
+            print("ERROR: Contents of ARGUMENTS_DESCR and respective class properties do not match!")
+            return
+        
+        for arg in cli_args:
+            if arg.action:
+                _ = self._parser.add_argument(
+                    arg.shortForm,
+                    arg.longForm,
+                    action=arg.action
+                )
+
+            elif arg.argumentType:
+                _ = self._parser.add_argument(
+                    arg.shortForm,
+                    arg.longForm,
+                    type=arg.argumentType
+                )
+            
+            else:
+                print("ERROR: argument description needs either type or action")
+                return
+        
+        _ = self._parser.parse_args(namespace=self)
+
+    def fn_get_module_props_in_dict(self) -> dict[str, str | bool]:
         return {
             'name': self.name,
             'app': self.app,
@@ -52,7 +187,7 @@ class FTArguments:
             'category': self.category,
         }
     
-    def get_model_props_in_dict(self):
+    def fn_get_model_props_in_dict(self):
         return {
             'name': self.name,
             'model_type': self.model_type,
