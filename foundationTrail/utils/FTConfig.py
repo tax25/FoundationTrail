93e4434ae2c3889eee3a882c3f8fddc03fde7ddb
tax25 <andreatax25@gmail.com>
2026-06-18 22:24:48 +0200

    started writing odoo configuration file parser

--- foundationTrail/utils/FTConfig.py
+++ foundationTrail/utils/FTConfig.py
@@ -1,6 +1,8 @@
 from os.path import expanduser, isfile
 from tomllib import load as load_toml
 from typing import override
+from re import search as regex_search
+
 
 FT_CONFIG_FILE_PATH = '~/.config/FoundationTrail/config.toml'
 CONFIGURATION_LABEL = 'configuration'
@@ -9,6 +11,8 @@ OPTIONS_LABEL = 'options'
 ERR_ODOO_CONF_PATH_NON_EXISTENT = "The path specified for the Odoo configuration file ('{path}') is *non* existent."
 ERR_ODOO_CONF_FILE_NOT_VALID = 'The contents of the configuration file specified make up for an invalid configuration.'
 
+SECTION_NAME_REGEX = r'^\[([a-zA-Z_-]+)\]$'
+
 class OdooConfPathNonExistentError(BaseException):
     pass
 
@@ -16,6 +20,8 @@ class OdooConfFileNotValid(BaseException):
     pass
 
 class FTConfig:
+    # TODO: review naming convention.
+    # This already looks confusing.
     conf_odoo_conf_path: str = ''
     conf_odoo_conf: OdooConf
 
@@ -51,17 +57,41 @@ class FTConfig:
 
 class OdooConf:
     addon_paths: list[str]
-
+    
     def __init__(self, conf_path: str):
         if not isfile(conf_path):
             raise OdooConfPathNonExistentError(ERR_ODOO_CONF_PATH_NON_EXISTENT.format(path=conf_path))
+        
+        with open(conf_path, 'r') as conf_file:
+            config_dict: dict[str, dict[str, str]] = {}
+            current_section: str = ""
 
-        with open(conf_path, 'rb') as conf_file:
-            conf_values = load_toml(conf_file)
-            if OPTIONS_LABEL not in conf_values:
-                raise OdooConfFileNotValid(ERR_ODOO_CONF_FILE_NOT_VALID)
+            for config_line in conf_file:
+                
+                if config_line.startswith(';'):
+                    # Then it means it is a comment, and should **not** be considered.
+                    continue
+                if config_line.startswith('['):
+                    if new_section_name := regex_search(SECTION_NAME_REGEX, config_line.strip()):
+                        current_section = new_section_name.group(1)
+                        # NOTE: once we get the new section, we go on, as there
+                        # are no actual values in this line.
+                        continue
 
-            addons_path: str = conf_values['options'].get('addons_path', '') # pyright: ignore[reportAny]
-            addons_paths: list[str] = addons_path.split(',')
+                if not '=' in config_line and not config_line.startswith('['):
+                    raise OdooConfFileNotValid(ERR_ODOO_CONF_FILE_NOT_VALID)
 
-            self.addon_paths = addons_paths
+                config_line_name, config_line_value = config_line.split('=')
+                
+                if not current_section in config_dict:
+                    config_dict[current_section] = {
+                        config_line_name.strip(): config_line_value
+                    }
+                else:
+                    config_dict[current_section][config_line_name.strip()] = config_line_value
+
+            print(config_dict) 
+
+
+if __name__ == '__main__':
+    my_config: FTConfig = FTConfig()
